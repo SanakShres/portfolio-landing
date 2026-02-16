864a0554b832fd19e2695aebaed66257522d463c
Sanak Shrestha <sanakstha@gmail.com>
2026-02-16 14:57:33 +0545

    minor change

--- src/components/landing.component.tsx
+++ src/components/landing.component.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { motion, Variants } from "framer-motion";
+
+const slideUp: Variants = {
+	initial: {
+		y: 300,
+	},
+	enter: {
+		y: 0,
+		transition: { duration: 0.6, ease: [0.33, 1, 0.68, 1], delay: 2.5 },
+	},
+};
 
 export default function PortfolioLanding() {
-	return <section className="relative flex h-dvh overflow-hidden"></section>;
+	return (
+		<motion.section
+			variants={slideUp}
+			initial="initial"
+			animate="enter"
+			className="relative flex h-dvh overflow-hidden"
+		></motion.section>
+	);
 }
